6a0c6c3e30c3a75bd8b373b815a332f22226b73f
Abigail <abi25.fly@gmail.com>
2017-07-25 10:51:20 -0500

    Actualizado

--- venPasajes.js
+++ venPasajes.js
@@ -23,10 +23,11 @@ function mostrarReserva (){
     pax[asiento]={
     nombre:nombre,
     apellidos:apellidos,
-    dni:dni
+    dni:dni,
+    asiento:asiento
     }
   console.log(pax);
-  mostrarDatos.innerHTML="<center><h3>Datos de la Reserva</h3><p><strong>Número de asiento: </strong>"+asiento+"</p><p><strong>Nombre del Pasajero: </strong>" +nombre+ ", " +apellidos+"</p><p><strong>DNI del pasajero: </strong>"+dni+"</p></center>"
+  mostrarDatos.innerHTML="<center><h3>Datos de la Reserva</h3><p><strong>Número de asiento: </strong>"+asiento+"</p><p><strong>Nombre del Pasajero: </strong>" +nombre+ ", " +apellidos+"</p><p><strong>DNI del pasajero: </strong>"+dni+"</p><br></center>"
   borrar();
 }
 
@@ -35,23 +36,24 @@ function borrar(){
   document.getElementById("inputSurname").value="";
   document.getElementById("inputDni").value="";
 }
-function buscar_dni(){
-  var busDni=document.getElementById("buscardni").value;
-  for(var i = 1; i<=32;i++){
-    if(pax[i] != undefined && pax[i].dni == busDni){
-      document.getElementById("inputName").value = pax[i].nombre;
-      document.getElementById("inputSurname").value = pax[i].apellidos;
-      document.getElementById("inputDni").value = pax[i].dni;
-    }
-  }
-}
 
 function muestraLista(){
   for(var i = 1; i<=32;i++){
     if(pax[i] != undefined){
-      var nombre=document.getElementById("inputName").value += pax[i].nombre;
-      var apellidos=document.getElementById("inputSurname").value += pax[i].apellidos;
-      var dni=document.getElementById("inputDni").value += pax[i].dni;
+      var list = document.getElementById("checklist");
+      var showlist = document.getElementById("listar");
+      list.innerHTML="<center><h3>Lista de pasajeros: </h3></center>"
+      showlist.innerHTML+="<center><p>Número de asiento: "+pax[i].asiento+"</p><p>Nombre del pasajero: "+pax[i].nombre+", "+pax[i].apellidos+"</p><p>DNI del pasajero: "+pax[i].dni+"</p><br></center>"
+    }
+  }
+}
+
+function buscar(){
+  var busDni=document.getElementById("buscardni").value;
+  for(var i = 1; i<=32;i++){
+    if(pax[i] != undefined && pax[i].dni == busDni){
+      var findDni =document.getElementById("muestraDNI");
+      findDni.innerHTML="<center><h3>Reserva Encontrada</h3><p>Número de asiento: "+pax[i].asiento+"</p><p>Nombre del pasajero: "+ pax[i].nombre+", "+pax[i].apellidos+"</p><p>DNI del pasajero: "+pax[i].dni+"</p></center>"
     }
   }
 }
